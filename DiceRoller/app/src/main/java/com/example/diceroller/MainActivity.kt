07711c979f87177248d872a2ff16c2f3a5d57ddf
Teslime <teslime_mlpnr@outlook.com>
2023-06-07 13:47:56 +0300

    Added random number when button click | DiceRoller

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,6 +4,10 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.Random
+import kotlin.random.Random as Random1
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -12,6 +16,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var rollbutton: Button = findViewById(R.id.roll_button)
-        rollbutton.text = "Let's Roll"
+        rollbutton.setOnClickListener {
+           rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        val randomInt = Random().nextInt(6) + 1
+        resultText.text = randomInt.toString()
     }
 }
