9e6aefc1e58dbdce1c7c2bf41c048c5d7b9f6058
Teslime <teslime_mlpnr@outlook.com>
2023-06-07 11:55:56 +0300

    Added button action | DiceRoller

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.diceroller
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var rollbutton: Button = findViewById(R.id.roll_button)
+        rollbutton.text = "Let's Roll"
     }
 }
